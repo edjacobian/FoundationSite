f6b570ffcfc5c757ae47e637994040aed833224a
Eddie Jacobian <edjacobian@gmail.com>
2024-04-02 15:00:55 -0400

    removed groot gif

--- FoundationSite/src/About.jsx
+++ FoundationSite/src/About.jsx
@@ -1,11 +1,6 @@
 const About = () => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <img
-          src="https://d23.com/app/uploads/2022/08/08_Groot-9.gif"
-          alt="Groot dancing GIF"
-          style={{ maxWidth: '100%', maxHeight: '100%' }} // Adjust maxHeight as needed
-        />
+    <div> 
       </div>
   );
 }
